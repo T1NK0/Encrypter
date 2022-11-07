e1b7b45f32e2a9e6bdce1a3138182fe98e67f26f
Mads Tinko Madsen <mtm@itklyngen.dk>
2022-11-07 14:03:53 +0100

    Add project files.

---
+++ Encrypter/Program.cs
@@ -0,0 +1,22 @@
+﻿List<string> alphabet = new List<string> {"A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "Æ", "Ø", "Å"};
+
+string userInput = Console.ReadLine();
+List<string> originalInput = new List<string>();
+
+for (int i = 0; i < userInput.Length; i++)
+{
+    originalInput.Add(userInput[i].ToString().ToUpper());
+}
+
+List<string> encrypt = new List<string>();
+for (int i = 0; i < originalInput.Count; i++)
+{
+    int getOriginalInputCharPosition = alphabet.IndexOf(originalInput[i]);
+
+    if (getOriginalInputCharPosition != alphabet.Count);
+    {
+        int getNewInputCharPosition = alphabet + 1;
+    }
+
+    Console.WriteLine(getOriginalInputCharPosition);
+}
